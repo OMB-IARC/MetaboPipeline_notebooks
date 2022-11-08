65a8bd921df21c09b2b31f4722dc85a10b86dba0
maxvincent24 <maxime.vincent@philiaweb.com>
2022-11-08 17:22:48 +0100

    Remove seaborn options from this script

--- bin/dimensionality_reduction_functions.py
+++ bin/dimensionality_reduction_functions.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import seaborn as sns
 
 
 from sklearn.preprocessing import StandardScaler
@@ -12,18 +14,6 @@ import time
 
 
 
-import seaborn as sns
-# Tune the visual settings for figures in seaborn
-sns.set_context(
-    "notebook", font_scale=1.2, rc={"axes.titlesize": 15, 'figure.figsize': (15,10)}
-)
-
-from matplotlib import rcParams
-
-sns.set_theme(style="darkgrid")
-
-rcParams['figure.figsize'] = 15,10
-
 
 
 
@@ -33,7 +23,8 @@ rcParams['figure.figsize'] = 15,10
 from sklearn.decomposition import PCA
 
 '''
-Perform PCA on a peak table
+Perform PCA on a peak table.
+
 
 input :
     - X : peakTable with only variable columns, no metadata
@@ -41,6 +32,7 @@ input :
     - part_explained_variance : part of initial explained variance we want to keep in the reduced dimensions
     - concat_metadata (default=False) : if set to True, the returned dataframe df is concatenated with metadata passed as argument
     - metadata (default=None) : required if concat_metadata is set to True, corresponds to metadata dataframe
+    - scale_before_PCA (default=False) : whether to apply standard scaler to the input peak table X before performing PCA
 return :
     - df : dataframe with PCA components as columns and samples as rows
     - explained_variance : dataframe of % explained variance and cumulative sum for each component
@@ -72,7 +64,7 @@ def perform_PCA(X, n_components=None, part_explained_variance=None, concat_metad
         # assert metadata != None
         assert not isinstance(metadata, type(None)),\
             'If <merge_metadata> is set to True, <metadata> has to be provided'
-        
+    
         
     # Perform PCA analysis
     if not isinstance(n_components, type(None)):
@@ -108,12 +100,12 @@ def perform_PCA(X, n_components=None, part_explained_variance=None, concat_metad
     
     
     # Plot cumulative sum of explained variance
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(12,8))
     xi = np.arange(1, explained_variance.shape[0] + 1, step=1)
     y = np.cumsum(PCA_model.explained_variance_ratio_)
 
-    plt.ylim(0, 1.05)
     plt.plot(xi, y, marker='o', linestyle='--', color='b')
+    plt.ylim(0, 1.05)
     plt.text(x=PCA_model.n_components_, y=y[-1] + 0.03, s=f'PC{PCA_model.n_components_}\n({y[-1]*100:.2f}%)',
              color='b', fontsize=12)
 
@@ -128,6 +120,12 @@ def perform_PCA(X, n_components=None, part_explained_variance=None, concat_metad
         plt.text(x=0.5, y=part_explained_variance - 0.05, s=f'{part_explained_variance}% cut-off threshold', color='r', fontsize=12)
 
     plt.show()
+    
+    
+    # Print information about number of dimensions before and after PCA
+    print(f'Initial number of dimension : {X.shape[1]}')
+    print(f'Final number of dimension : {explained_variance.shape[0]}')
+    
     
     return subset, explained_variance
 
@@ -163,13 +161,13 @@ def select_best_features(X, y, k=None, alpha=None, concat_metadata=False, metada
     f_statistic, p_values = f_classif(X, y)
 
     # Create dataframe with f_score and p_value for each feature
-    df_f_classif = pd.DataFrame(list(zip(X.columns.values, f_statistic, p_values)), columns=['features', 'f_scores', 'p_values'])\
-    .sort_values(by ='f_scores', ascending=False)
-    df_f_classif = df_f_classif.round({'f_scores': 3, 'p_values': 4})
+    df_f_classif = pd.DataFrame(list(zip(X.columns.values, f_statistic, p_values)), columns=['features', 'F-value', 'p-value'])\
+    .sort_values(by ='F-value', ascending=False)
+    #df_f_classif = df_f_classif.round({'F-value': 3, 'p-value': 4})
     
     # Subset df_f_classif based on k and p_value
     df_f_classif = df_f_classif.iloc[:k,:]
-    df_f_classif = df_f_classif[df_f_classif['p_values'] < alpha]
+    df_f_classif = df_f_classif[df_f_classif['p-value'] < alpha]
     
     # Subset X with selected features
     subset = X[df_f_classif['features'].values]
@@ -177,6 +175,10 @@ def select_best_features(X, y, k=None, alpha=None, concat_metadata=False, metada
     # Concat metadata if set to True
     if concat_metadata:
         subset = pd.concat([metadata, subset], axis=1)
+        
+    print(f'Initial number of dimension : {X.shape[1]}')
+    print(f'Final number of dimension : {df_f_classif.shape[0]}')
+
     
     return subset, df_f_classif
 
@@ -191,9 +193,9 @@ def select_percentile_features(X, y, percentile=100, concat_metadata=False, meta
     f_statistic, p_values = f_classif(X, y)
 
     # Create dataframe with f_score and p_value for each feature
-    df_f_classif = pd.DataFrame(list(zip(X.columns.values, f_statistic, p_values)), columns=['features', 'f_scores', 'p_values'])\
-    .sort_values(by ='f_scores', ascending=False)
-    df_f_classif = df_f_classif.round({'f_scores': 3, 'p_values': 4})
+    df_f_classif = pd.DataFrame(list(zip(X.columns.values, f_statistic, p_values)), columns=['features', 'F-value', 'p-value'])\
+    .sort_values(by ='F-value', ascending=False)
+    #df_f_classif = df_f_classif.round({'F-value': 3, 'p-value': 4})
     
     # Subset X with selected features
     selector = SelectPercentile(f_classif, percentile=percentile)
@@ -209,9 +211,37 @@ def select_percentile_features(X, y, percentile=100, concat_metadata=False, meta
     # Concat metadata if set to True
     if concat_metadata:
         subset = pd.concat([metadata, subset], axis=1)
+        
+    print(f'Initial number of dimension : {X.shape[1]}')
+    print(f'Final number of dimension : {df_f_classif.shape[0]}')
+
     
     return subset, df_f_classif
 
+
+
+
+
+
+def plot_pvalue_fvalue(feature_scores):
+    
+    if len(feature_scores) > 75:
+        print('Dataframe has too much features to display plots (75 features is the maximum length accepted).')
+
+    else:
+        plt.figure(figsize=(12, 8))
+        ax = sns.barplot(data=feature_scores, x='features', y='p-value');
+        plt.xticks(rotation=90)
+        ax.set_title('p-value for each selected feature', fontsize=16)
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        plt.show()
+
+        plt.figure(figsize=(12, 8))
+        ax = sns.barplot(data=feature_scores, x='features', y='F-value');
+        plt.xticks(rotation=90)
+        ax.set_title('ANOVA F-value for each selected feature', fontsize=16)
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        plt.show()
 
 
 
@@ -224,13 +254,15 @@ def select_percentile_features(X, y, percentile=100, concat_metadata=False, meta
 from sklearn.manifold import TSNE
 
 
-def perform_tSNE(X, n_components=2, targets_plot=[], concat_metadata=False, metadata=None):
+def perform_tSNE(X, n_components=2, concat_metadata=False, metadata=None, scale_before_tSNE=False, target=None):
     
     # Apply StandardScaler() if not already done
-    if (X.mean().mean() < 0.01) & (0.99 < X.std().mean() < 1.01):
-        X_std = X
-    else:
-        X_std = StandardScaler().fit_transform(X)
+    #if (X.mean().mean() < 0.01) & (0.99 < X.std().mean() < 1.01):
+    #    X_std = X
+    #else:
+    #    X_std = StandardScaler().fit_transform(X)
+    if scale_before_tSNE:
+        X = StandardScaler().fit_transform(X)
         
         
     # Assertion
@@ -240,8 +272,8 @@ def perform_tSNE(X, n_components=2, targets_plot=[], concat_metadata=False, meta
     
     # Perform t-SNE
     start_time = time.time()
-    X_tSNE = TSNE(n_components=n_components, learning_rate='auto', init='random', random_state=0).fit_transform(X_std)
-    print(f'Time to compute t-SNE (input dataframe of shape {X_std.shape}) : {time.time() - start_time :.2f} seconds\n')
+    X_tSNE = TSNE(n_components=n_components, learning_rate='auto', init='random', random_state=0).fit_transform(X)
+    print(f'Time to compute t-SNE (input dataframe of shape {X.shape}) : {time.time() - start_time :.2f} seconds\n')
     
     # Prepare column names
     col_names = ['tSNE' + str(i+1) for i in range(n_components)]
@@ -260,7 +292,7 @@ def perform_tSNE(X, n_components=2, targets_plot=[], concat_metadata=False, meta
     if n_components == 2:
         
         print(100 * '-')
-        if targets_plot==[]:
+        if isinstance(target, type(None)):
             plt.figure(figsize=(12,8))
             sns.scatterplot(x=0, y=1, data=pd.DataFrame(X_tSNE),
                             s=50, legend='full', palette='deep');
@@ -270,21 +302,19 @@ def perform_tSNE(X, n_components=2, targets_plot=[], concat_metadata=False, meta
             plt.show()
             print(100 * '-')
         else:
-            for target in targets_plot:
-
-                plt.figure(figsize=(12,8))
-                sns.scatterplot(x=0, y=1, data=pd.DataFrame(X_tSNE), hue=metadata[target],
-                                s=50, legend='full', palette='deep');
-                plt.xlabel('t-SNE (1st dimension)', fontsize=14)
-                plt.ylabel('t-SNE (2nd dimension)', fontsize=14)
-                plt.title(f't-SNE first two components, colored by {target}', fontsize=18)
-                plt.show()
-                print(100 * '-')
+            plt.figure(figsize=(12,8))
+            sns.scatterplot(x=0, y=1, data=pd.DataFrame(X_tSNE), hue=target,
+                            s=50, legend='full', palette='deep');
+            plt.xlabel('t-SNE (1st dimension)', fontsize=14)
+            plt.ylabel('t-SNE (2nd dimension)', fontsize=14)
+            plt.title(f't-SNE first two components, colored by {target.name}', fontsize=18)
+            plt.show()
+            print(100 * '-')
             
     elif n_components == 3:
         
         print(100 * '-', '\n')
-        if targets_plot==[]:
+        if isinstance(target, type(None)):
             fig = plt.figure(figsize=(15,10))
             ax = fig.add_subplot(111, projection = '3d')
 
@@ -304,41 +334,37 @@ def perform_tSNE(X, n_components=2, targets_plot=[], concat_metadata=False, meta
             plt.show()
             
         else :
-            for target in targets_plot:
+            fig = plt.figure(figsize=(15,10))
+            ax = fig.add_subplot(111, projection = '3d')
 
-                fig = plt.figure(figsize=(15,10))
-                ax = fig.add_subplot(111, projection = '3d')
+            le = LabelEncoder()
+            le.fit(target)
+            encoded_labels = le.transform(target)
+            le_name_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+            print(f'Corresponding labels for each class in legend : {le_name_mapping}\n')
 
-                le = LabelEncoder()
-                le.fit(metadata[target])
-                encoded_labels = le.transform(metadata[target])
-                le_name_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
-                print(f'Corresponding labels for each class in legend : {le_name_mapping}\n')
+            scatter = ax.scatter(
+                xs=X_tSNE[:,0], 
+                ys=X_tSNE[:,1], 
+                zs=X_tSNE[:,2],
+                c=encoded_labels,
+                cmap='rainbow',
+                s=50,
+                alpha=1
+            )
+            ax.set_xlabel('t-SNE (1st dimension)')
+            ax.set_ylabel('t-SNE (2nd dimension)')
+            ax.set_zlabel('t-SNE (3rd dimension)')
+            ax.set_title(f't-SNE first 3 components, colored by {target.name}', fontsize=18)
 
-                scatter = ax.scatter(
-                    xs=X_tSNE[:,0], 
-                    ys=X_tSNE[:,1], 
-                    zs=X_tSNE[:,2],
-                    c=encoded_labels,
-                    cmap='rainbow',
-                    s=50,
-                    alpha=1
-                )
-                ax.set_xlabel('t-SNE (1st dimension)')
-                ax.set_ylabel('t-SNE (2nd dimension)')
-                ax.set_zlabel('t-SNE (3rd dimension)')
-                ax.set_title(f't-SNE first 3 components, colored by {target}', fontsize=18)
+            legend1 = ax.legend(*scatter.legend_elements(), loc='upper right', title=target.name)
+            ax.add_artist(legend1)
 
-                legend1 = ax.legend(*scatter.legend_elements(), loc='upper right', title=target)
-                ax.add_artist(legend1)
+            plt.show()
 
-                plt.show()
-
-            print(100 * '-', '\n')
+        print(100 * '-', '\n')
             
     return df_tSNE
-
-
 
 
 
@@ -360,13 +386,15 @@ def perform_tSNE(X, n_components=2, targets_plot=[], concat_metadata=False, meta
 from umap import UMAP
 
 
-def perform_UMAP(X, metadata, n_components=2, targets_plot=[], concat_metadata=False):
+def perform_UMAP(X, n_components=2, concat_metadata=False, metadata=None, scale_before_UMAP=False, target=None):
     
     # Apply StandardScaler() if not already done
-    if (X.mean().mean() < 0.01) & (0.99 < X.std().mean() < 1.01):
-        X_std = X
-    else:
-        X_std = StandardScaler().fit_transform(X)
+    #if (X.mean().mean() < 0.01) & (0.99 < X.std().mean() < 1.01):
+    #    X_std = X
+    #else:
+    #    X_std = StandardScaler().fit_transform(X)
+    if scale_before_UMAP:
+        X = StandardScaler().fit_transform(X)
         
         
     # Assertion
@@ -376,8 +404,8 @@ def perform_UMAP(X, metadata, n_components=2, targets_plot=[], concat_metadata=F
     
     # Perform UMAP
     start_time = time.time()
-    X_UMAP = UMAP(n_components=n_components, init='random', random_state=0).fit_transform(X_std)
-    print(f'Time to compute UMAP (input dataframe of shape {X_std.shape}) : {time.time() - start_time :.2f} seconds\n')
+    X_UMAP = UMAP(n_components=n_components, init='random', random_state=0).fit_transform(X)
+    print(f'Time to compute UMAP (input dataframe of shape {X.shape}) : {time.time() - start_time :.2f} seconds\n')
 
     
     
@@ -398,7 +426,7 @@ def perform_UMAP(X, metadata, n_components=2, targets_plot=[], concat_metadata=F
     if n_components == 2:
         
         print(100 * '-')
-        if targets_plot==[]:
+        if isinstance(target, type(None)):
             plt.figure(figsize=(12,8))
             sns.scatterplot(x=0, y=1, data=pd.DataFrame(X_UMAP),
                             s=50, legend='full', palette='deep');
@@ -408,21 +436,19 @@ def perform_UMAP(X, metadata, n_components=2, targets_plot=[], concat_metadata=F
             plt.show()
             print(100 * '-')
         else:
-            for target in targets_plot:
-
-                plt.figure(figsize=(12,8))
-                sns.scatterplot(x=0, y=1, data=pd.DataFrame(X_UMAP), hue=metadata[target],
-                                s=50, legend='full', palette='deep');
-                plt.xlabel('UMAP (1st dimension)', fontsize=14)
-                plt.ylabel('UMAP (2nd dimension)', fontsize=14)
-                plt.title(f'UMAP first two components, colored by {target}', fontsize=18)
-                plt.show()
-                print(100 * '-')
+            plt.figure(figsize=(12,8))
+            sns.scatterplot(x=0, y=1, data=pd.DataFrame(X_UMAP), hue=target,
+                            s=50, legend='full', palette='deep');
+            plt.xlabel('UMAP (1st dimension)', fontsize=14)
+            plt.ylabel('UMAP (2nd dimension)', fontsize=14)
+            plt.title(f'UMAP first two components, colored by {target.name}', fontsize=18)
+            plt.show()
+            print(100 * '-')
             
     elif n_components == 3:
         
         print(100 * '-', '\n')
-        if targets_plot==[]:
+        if isinstance(target, type(None)):
             fig = plt.figure(figsize=(15,10))
             ax = fig.add_subplot(111, projection = '3d')
 
@@ -442,36 +468,34 @@ def perform_UMAP(X, metadata, n_components=2, targets_plot=[], concat_metadata=F
             plt.show()
             
         else :
-            for target in targets_plot:
+            fig = plt.figure(figsize=(15,10))
+            ax = fig.add_subplot(111, projection = '3d')
 
-                fig = plt.figure(figsize=(15,10))
-                ax = fig.add_subplot(111, projection = '3d')
+            le = LabelEncoder()
+            le.fit(target)
+            encoded_labels = le.transform(target)
+            le_name_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+            print(f'Corresponding labels for each class in legend : {le_name_mapping}\n')
 
-                le = LabelEncoder()
-                le.fit(metadata[target])
-                encoded_labels = le.transform(metadata[target])
-                le_name_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
-                print(f'Corresponding labels for each class in legend : {le_name_mapping}\n')
+            scatter = ax.scatter(
+                xs=X_UMAP[:,0], 
+                ys=X_UMAP[:,1], 
+                zs=X_UMAP[:,2],
+                c=encoded_labels,
+                cmap='rainbow',
+                s=50,
+                alpha=1
+            )
+            ax.set_xlabel('UMAP (1st dimension)')
+            ax.set_ylabel('UMAP (2nd dimension)')
+            ax.set_zlabel('UMAP (3rd dimension)')
+            ax.set_title(f'UMAP first 3 components, colored by {target.name}', fontsize=18)
 
-                scatter = ax.scatter(
-                    xs=X_UMAP[:,0], 
-                    ys=X_UMAP[:,1], 
-                    zs=X_UMAP[:,2],
-                    c=encoded_labels,
-                    cmap='rainbow',
-                    s=50,
-                    alpha=1
-                )
-                ax.set_xlabel('UMAP (1st dimension)')
-                ax.set_ylabel('UMAP (2nd dimension)')
-                ax.set_zlabel('UMAP (3rd dimension)')
-                ax.set_title(f'UMAP first 3 components, colored by {target}', fontsize=18)
+            legend1 = ax.legend(*scatter.legend_elements(), loc='upper right', title=target.name)
+            ax.add_artist(legend1)
 
-                legend1 = ax.legend(*scatter.legend_elements(), loc='upper right', title=target)
-                ax.add_artist(legend1)
+            plt.show()
 
-                plt.show()
-
-            print(100 * '-', '\n')
+        print(100 * '-', '\n')
             
     return df_UMAP
